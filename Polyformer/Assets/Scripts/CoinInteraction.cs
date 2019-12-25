94c5981b80d105155afe8ba0fbd118c70210f546
TwoEyedMikeWazowski <danielsyrov9@gmail.com>
2019-12-25 21:45:14 +0800

    Coins implemented
    Now we just need the store itself

---
+++ Polyformer/Assets/Scripts/CoinInteraction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinInteraction : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.name == "Player") {
+
+            PlayerMovement.coins += 1;
+            Debug.Log(PlayerMovement.coins);
+            Destroy(gameObject);
+        }
+    }
+}
