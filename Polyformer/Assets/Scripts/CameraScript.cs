a4fbb83f7c688b3dcedb28275c31e3bb7c6e06ca
TwoEyedMikeWazowski <danielsyrov9@gmail.com>
2020-01-04 16:43:33 +0800

    Fixed camera cutting out stuff

---
+++ Polyformer/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float x = 15.1497600001f / Screen.width * Screen.height;
+        Camera.main.orthographicSize = x;
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
